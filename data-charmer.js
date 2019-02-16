98a5cdc631ee58b9be9aba3eae719823ee146b84
Steve Barriere <stevebarriere.student@careerdevs.com>
2019-02-16 02:07:08 +0000

    Created a for loop to show only employee number and department

--- data-charmer.js
+++ data-charmer.js
@@ -1,24 +1,14 @@
 var fs = require('fs');
 
-  let newArr = [];
-  let newArr2 = [];
-  
+let employeeNumber = '';
+
 fs.readFile('employFile2.txt', 'utf8', function(err, data) {
   if (err) throw err;
 
-  let arraydat = data.split('\n');
-
-  let arr = arraydat;
-
-
-
-  while (arr.length) newArr.push(arr.splice(0, 1));
-
-  for (var i = 0; i < newArr.length; i++) {
-    console.log(newArr[i]);
-    newArr.splice(0, 1);
-
+  employeeNumber = data.split('\n')
+  for (let i = 0; i < employeeNumber.length; i++) {
+    for (let j = 0; j < employeeNumber[i].length; j++);
+    console.log(employeeNumber[i].substring(0, 6).replace(/[(,),']/g, '') + ' ' + employeeNumber[i].substring(12, 6).replace(/[(,),']/g, ''));
   }
-  
-})
 
+})
